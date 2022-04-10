9370a68936b90c50109f6d7dc618b93286282c02
Andromanov <96685721+Andromanov@users.noreply.github.com>
2022-04-10 14:28:39 +0600

    first commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import './index.css'
 import 'antd/dist/antd.css';
 import { App } from "./App"
 
-export const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjJiNmZmYzA5YjEyZjgwZjRjMTBiYjQiLCJpYXQiOjE2NDcwMTM4ODYsImV4cCI6MTY3ODU0OTg4Nn0.ZRNgpbPfTYd1PoDqcgCMEHC32g6IYkvklY0tMMil2do'
+
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
